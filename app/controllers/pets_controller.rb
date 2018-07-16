d6fce7319d9d53e0e1922bb6cce4f70bee2b1132
Cristina Lugo <cmlugoce@syr.edu>
2018-07-16 02:00:35 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,12 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect "pets/#{@pet.id}"
     redirect to "pets/#{@pet.id}"
   end
 
