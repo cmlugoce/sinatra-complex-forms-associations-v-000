7792ae0466b495d47ffae45c5b1c043a371f8c26
Cristina Lugo <cmlugoce@syr.edu>
2018-07-16 02:19:43 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,30 +6,36 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
-+    @pets = Pet.all
-     erb :'/owners/new'
-   end
- 
-   post '/owners' do 
--    
-+    @owner = Owner.create(params[:owner])
-+    if !params["pet"]["name"].empty?
-+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    @pets = Pet.all
+    erb :'/owners/new'
+  end
+
+  post '/owners' do 
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     @owner.save
     redirect "owners/#{@owner.id}"
-   end
- 
-   get '/owners/:id/edit' do 
- end
- 
-   post '/owners/:id' do 
-   
+  end
+
+  get '/owners/:id/edit' do 
+    @owner = Owner.find(params[:id])
+    @pets = Pet.all
+    erb :'/owners/edit'
+  end
+
+  get '/owners/:id' do 
+    @owner = Owner.find(params[:id])
+    erb :'/owners/show'
+  end
+
+  post '/owners/:id' do 
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
-    redirect to "owners/#{@owner.id}"
-   end
- end 
+    redirect "owners/#{@owner.id}"
+  end
+end
